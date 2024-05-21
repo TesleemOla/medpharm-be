fe037c58df3cc4637a7817ea95f320585b4fadc2
Tasleemofx <oladepotesleem5@gmail.com>
2024-05-21 11:02:08 +0100

    added cors package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan"
 import helmet from "helmet"
+import cors from 'cors'
 
 // import middlewares
 import errorHandler from "./middlewares/errorHandler.js"
@@ -26,6 +27,7 @@ Connect()
 
 // middlewares
 app.use(helmet())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan("dev"))
